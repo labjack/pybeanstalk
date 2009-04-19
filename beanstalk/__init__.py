d3a3256fb3c8ae1ad7c436ba23d292e0bae35607
Jeethu Rao <jeethurao@jeethu-raos-macbook.local>
2009-04-19 05:40:48 +0530

    __all__ should contain strings, not object references

--- beanstalk/__init__.py
+++ beanstalk/__init__.py
@@ -2,9 +2,9 @@ import serverconn
 import job
 import errors
 import protohandler
-__all__ = [protohandler, serverconn, errors, job]
+__all__ = ['protohandler', 'serverconn', 'errors', 'job']
 try:
     import twisted_client
-    __all__.append[twisted_client]
+    __all__.append('twisted_client')
 except ImportError:
     pass
